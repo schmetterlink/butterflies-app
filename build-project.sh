c96599e44c826e83fad7aa42289050c980e86cad
Timor Kodal <timor@kodal.de>
2021-05-18 10:56:49 +0200

    assume YES when running apt-get

--- build-project.sh
+++ build-project.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get install npm
+sudo apt-get -y install npm
 npm install -g n
 sudo n stable
 sudo apt-get --assume-yes install curl
@@ -9,7 +9,7 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 sudo apt-get update
-sudo apt-get install yarn
+sudo apt-get -y install yarn
 yarn install
 yarn encore dev
 
