31b4804de3d0c7d0fb114b74fd55f786469b645d
Timor Kodal <timor@kodal.de>
2021-05-18 12:15:32 +0200

    replace user placeholders

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -25,33 +25,33 @@ class DefaultController extends AbstractController
         $users = [
             [
                 'id' => 1,
-                'name' => 'Olususi Oluyemi',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
-                'imageURL' => 'https://randomuser.me/api/portraits/women/50.jpg'
+                'name' => 'Timor Kodal',
+                'description' => 'Lorem Ipsum',
+                'imageURL' => 'build/images/tea-guevara.jpg'
             ],
             [
                 'id' => 2,
-                'name' => 'Camila Terry',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
-                'imageURL' => 'https://randomuser.me/api/portraits/men/42.jpg'
+                'name' => 'Hiu-Fai Lau',
+                'description' => '',
+                'imageURL' => '/images/hiu-fai.jpg'
             ],
             [
                 'id' => 3,
-                'name' => 'Joel Williamson',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
-                'imageURL' => 'https://randomuser.me/api/portraits/women/67.jpg'
+                'name' => 'Johanna Reichenbach',
+                'description' => '',
+                'imageURL' => '/images/johanna.jpg'
             ],
             [
                 'id' => 4,
-                'name' => 'Deann Payne',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
-                'imageURL' => 'https://randomuser.me/api/portraits/women/50.jpg'
+                'name' => 'Linus Wetzel',
+                'description' => '',
+                'imageURL' => '/images/linus.jpg'
             ],
             [
                 'id' => 5,
-                'name' => 'Donald Perkins',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
-                'imageURL' => 'https://randomuser.me/api/portraits/men/89.jpg'
+                'name' => 'Gregory Tumoscheid',
+                'description' => '',
+                'imageURL' => '/images/greg.jpg'
             ]
         ];
         /** @var JsonResponse $response */
