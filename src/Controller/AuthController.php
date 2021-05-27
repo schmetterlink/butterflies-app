3f71774056a95c28de5c45610ca65c3f39057237
Timor Kodal <timor@kodal.de>
2021-05-27 16:03:06 +0200

    * add package "firebase/php-jwt" to composer.json (``composer require firebase/php-jwt``) * require php >= 8.0 * use Firebase\JWT instead of Namshi\Jose\JWT in AuthController.php * update composer- and symfony lock files

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -4,12 +4,12 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
-use Namshi\JOSE\JWT;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Firebase\JWT\JWT;
 
 class AuthController extends AbstractController
 {
