39864f9ff6286305c09c4a626631e1bca1b031ca
Timor Kodal <timor@kodal.de>
2021-06-03 12:08:27 +0200

    introduce api/me - action to return serialized user data

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,14 +2,25 @@
 
 namespace App\Controller;
 
+use JMS\Serializer\SerializationContext;
+use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Serializer as Serializer;
 
 class ApiController extends AbstractController
 {
+
+    private $serializer;
+
+    public function __construct(Serializer\SerializerInterface $serializer) {
+        $this->serializer = $serializer;
+    }
+
     #[Route('/api/null', name: 'api')]
     public function index(): Response
     {
@@ -26,5 +37,24 @@ class ApiController extends AbstractController
             'user' => $tokenStorage->getToken(),
             'message' => 'token storage works!',
         ]);
+    }
+    /**
+     * @Route("/api/me", name="testapi")
+     */
+    public function me(Request $request, TokenStorageInterface $tokenStorage): JsonResponse
+    {
+        $encoder = new Serializer\Encoder\JsonEncoder();
+        $defaultContext = [
+            Serializer\Normalizer\AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function ($object, $format, $context) {
+                return $object::class;
+            },
+        ];
+
+        $user = $tokenStorage->getToken()->getUser();
+
+        $userData = $this->serializer->serialize($user, 'json', $defaultContext);
+
+        $response = new JsonResponse($userData);
+        return $response;
     }
 }
