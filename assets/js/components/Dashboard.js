ba0dc1246b0581eccb8cbd3d64ff2178bd27676d
Timor Kodal <timor@kodal.de>
2021-06-03 16:30:06 +0200

    * implement error handling for requests * redirect to login page if request returns a 401 (unauthorized) * trigger page refresh after user project has been created

--- assets/js/components/Dashboard.js
+++ assets/js/components/Dashboard.js
@@ -15,36 +15,77 @@ class Dashboard extends Component {
 
     constructor() {
         super();
-        this.state = { user: window.REACT_SERVER_PROPS.user, loading: true, token: window.REACT_SERVER_PROPS.token, data: false};
+        this.state = {
+            user: window.REACT_SERVER_PROPS.user,
+            loading: true, token: window.REACT_SERVER_PROPS.token,
+            data: false,
+            hasError: false,
+            status: 200
+        };
+    }
+    componentDidCatch(error, info) {
+        // Display fallback UI
+        this.setState({ hasError: true });
+        // You can also log the error to an error reporting service
+        console.error("Error: "+error+" (info)");
     }
 
-    callApi(target, method = "GET", callback = null) {
+    callApi(target, method = "GET", callback = null, errorCallback = null) {
         let uri="/api/"+target;
-        console.log("trigger "+method+" request to API ("+uri+") with token "+this.state.token);
+        console.debug("trigger "+method+" request to API ("+uri+") with token "+this.state.token);
         axios.post(
             uri,
             {title: 'TestTest', description: 'description'},
             {headers: {'Authorization': `Bearer ${this.state.token}`}}
         ).then(result => {
             if (callback === null) {
-                this.setState({ success: true, loading: false, payload: result.data});
-                console.log(result.data);
+                console.debug(result.data);
+                this.setState({status: result.status, loading: false, payload: result.data});
             } else {
                 callback(result.data, this);
             }
-        })
+        }).catch(error => {
+            let status = 500;
+            if (errorCallback === null) {
+                if (error.response) {
+                    console.error("Request responded with an error ["+error.response.status+"].");
+                    console.debug(error.response);
+                    status = error.response.status;
+                } else if (error.request) {
+                    console.error("timeout")
+                    console.debug(error.request);
+                }
+                this.setState({status: status, success: false, loading: false, hasError: true});
+            } else {
+                console.debug(error);
+                console.error("Error occurred: " + error);
+            }
+        });
+
         return null;
     }
     componentDidMount() {
+        this.getUserData();
+    }
+    getUserData() {
         let callback = function(data, that) {
-            console.log("callback yay!");
-            that.setState({ success: true, loading: false, userData: JSON.parse(data)});
-            console.log(data);
+            let userData = JSON.parse(data);
+            console.debug(data);
+            console.debug("successfully retrieved user data with ["+userData.projects.length+"] projects");
+            that.setState({ status: data.status, success: true, loading: false, userData: userData});
         }
         this.callApi("me", "GET", callback);
     }
+    createProject() {
+        console.debug("creating project");
+        let callback = function(data, that) {
+            console.debug("created a new project");
+            that.getUserData();
+        }
+        this.callApi("project", "POST", callback);
+    }
     render() {
-        if(!this.state.user) {
+        if(!this.state.user || this.state.status === 401) {
             return <Redirect to={"/login"} />
         }
         this.title = "Welcome "+this.state.user.name;
@@ -63,8 +104,8 @@ class Dashboard extends Component {
                     </TableHead>
                 </Table>
                 <div>{ new NestedList().renderData("data", this.state.user) }</div>
-                <div><Button variant="contained" color="primary" onClick={this.callApi.bind(this,"project", "POST")}>create Project</Button></div>
-                {loading ? (
+                <div><Button variant="contained" color="primary" onClick={this.createProject.bind(this)}>create Project</Button></div>
+                {loading || this.state.userData === undefined ? (
                     <div className={'row text-center'}>
                         <span className="fa fa-spin fa-spinner fa-4x"></span>
                     </div>
