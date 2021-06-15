fa585ee905c998d24673feb9536960acc69a835f
Timor Kodal <timor@kodal.de>
2021-06-15 12:54:42 +0200

    introduce "delete"-action

--- src/Controller/CrudController.php
+++ src/Controller/CrudController.php
@@ -4,8 +4,12 @@
 namespace App\Controller;
 
 use App\Entity;
+use App\Repository\ProjectRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Entity\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,7 +18,7 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\VarDumper\VarDumper;
 
-class CrudController
+class CrudController extends AbstractController
 {
     /**
      * @Route("/api/project", name="api_crud_add", methods={"POST"})
@@ -38,5 +42,23 @@ class CrudController
         $manager->persist($project);
         $manager->flush();
         return new JsonResponse($project->getId());
+    }
+    /**
+     * @Route("/api/{entity}/{id}", name="api_crud_delete", methods={"DELETE"})
+     */
+    public function delete(string $entity, string $id, Request $request, TokenStorageInterface $tokenStorage, EntityManagerInterface $manager):JsonResponse
+    {
+        /** @var Entity\User $user */
+        $user = $tokenStorage->getToken()->getUser();
+
+        $id = $id ?: $request->request->get('id',$request->get('id'));
+        $entity = $entity ?: $request->request->get('entity',$request->get('entity'));
+
+        /** @var Entity\Project $project */
+        $project = $this->getDoctrine()->getRepository(Entity\Project::class)->find($id);
+
+        $manager->remove($project);
+        $manager->flush();
+        return new JsonResponse([]);
     }
 }
