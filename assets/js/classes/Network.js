36034c8a0d5d81d9ea7b8533a11c0178dc62ceaf
Timor Kodal <timor@kodal.de>
2021-06-15 10:16:16 +0200

    outsource existing callApi method to Network class

---
+++ assets/js/classes/Network.js
@@ -0,0 +1,70 @@
+import NestedList from "./NestedList";
+import axios from "axios";
+
+class Network {
+    token = undefined;
+    caller = undefined;
+    constructor(caller = undefined, token = undefined) {
+        this.state = {
+            loading: false,
+            data: false,
+            hasError: false,
+            status: undefined
+        };
+        this.setCaller(caller);
+        this.setToken(token);
+    }
+    setCaller(caller = undefined) {
+        this.caller = caller;
+    }
+    setToken(token = undefined) {
+        this.token = token;
+    }
+    setState(state = undefined) {
+        console.debug("setting network"+(this.caller ? ' (and caller '+typeof this.caller+")" : '')+" state...");
+        console.debug(state);
+        this.state = state;
+        if (this.caller && this.caller.setState) {
+            this.caller.setState(state);
+        }
+    }
+    callApi(target, data = undefined, method = "GET", callback = null, errorCallback = null) {
+        this.state.loading = true;
+        let uri="/api/"+target;
+        console.debug("trigger "+method+" request to API ("+uri+") with token "+this.token);
+        axios.post(
+            uri,
+            data,
+            {headers: {'Authorization': `Bearer ${this.token}`}}
+        ).then(result => {
+            if (callback === null) {
+                console.debug(result);
+                this.setState({status: result.status, loading: false, payload: result.data});
+            } else {
+                callback(result.data);
+            }
+        }).catch(error => {
+            let status = 500;
+            if (errorCallback === null) {
+                if (error.response) {
+                    console.error("Request responded with an error ["+error.response.status+"].");
+                    console.debug(error.response);
+                    status = error.response.status;
+                } else if (error.request) {
+                    console.error("timeout")
+                    console.debug(error.request);
+                }
+                console.error(error);
+                this.setState({status: status, success: false, loading: false, hasError: true});
+            } else {
+                console.debug("an error occurred. trying to call custom errorCallback function.")
+                errorCallback(error);
+            }
+        });
+
+        return null;
+    }
+
+}
+
+export default Network;
--- assets/js/components/Dashboard.js
+++ assets/js/components/Dashboard.js
@@ -6,22 +6,20 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
-import NestedList from '../classes/NestedList';
 import { Redirect } from 'react-router';
 import {Link} from "react-router-dom";
 import axios from 'axios';
+import NestedList from "../classes/NestedList";
+import Network from "../classes/Network";
 
 class Dashboard extends Component {
-
+    network = undefined;
     constructor() {
         super();
         this.state = {
-            user: window.REACT_SERVER_PROPS.user,
-            loading: true, token: window.REACT_SERVER_PROPS.token,
-            data: false,
-            hasError: false,
-            status: 200
+            user: window.REACT_SERVER_PROPS.user
         };
+        this.network = new Network(this, window.REACT_SERVER_PROPS.token);
     }
     componentDidCatch(error, info) {
         // Display fallback UI
@@ -30,59 +28,30 @@ class Dashboard extends Component {
         console.error("Error: "+error+" (info)");
     }
 
-    callApi(target, method = "GET", callback = null, errorCallback = null) {
-        let uri="/api/"+target;
-        console.debug("trigger "+method+" request to API ("+uri+") with token "+this.state.token);
-        axios.post(
-            uri,
-            {title: 'TestTest', description: 'description'},
-            {headers: {'Authorization': `Bearer ${this.state.token}`}}
-        ).then(result => {
-            if (callback === null) {
-                console.debug(result.data);
-                this.setState({status: result.status, loading: false, payload: result.data});
-            } else {
-                callback(result.data, this);
-            }
-        }).catch(error => {
-            let status = 500;
-            if (errorCallback === null) {
-                if (error.response) {
-                    console.error("Request responded with an error ["+error.response.status+"].");
-                    console.debug(error.response);
-                    status = error.response.status;
-                } else if (error.request) {
-                    console.error("timeout")
-                    console.debug(error.request);
-                }
-                this.setState({status: status, success: false, loading: false, hasError: true});
-            } else {
-                console.debug(error);
-                console.error("Error occurred: " + error);
-            }
-        });
-
-        return null;
-    }
     componentDidMount() {
         this.getUserData();
     }
     getUserData() {
-        let callback = function(data, that) {
+        var that = this;
+        let callback = function(data) {
             let userData = JSON.parse(data);
             console.debug(data);
             console.debug("successfully retrieved user data with ["+userData.projects.length+"] projects");
             that.setState({ status: data.status, success: true, loading: false, userData: userData});
         }
-        this.callApi("me", "GET", callback);
+        this.network.callApi("me", undefined,"GET", callback);
     }
     createProject() {
         console.debug("creating project");
-        let callback = function(data, that) {
+        var that = this;
+        let callback = function(data) {
             console.debug("created a new project");
             that.getUserData();
         }
-        this.callApi("project", "POST", callback);
+        this.network.callApi("project", {title: 'TestTestTest', description: 'lorem ipsissimum'}, "POST", callback);
+    }
+    editProfile() {
+        console.debug("edit user profile");
     }
     render() {
         if(!this.state.user || this.state.status === 401) {
@@ -90,7 +59,6 @@ class Dashboard extends Component {
         }
         this.title = "Welcome "+this.state.user.name;
         const loading = this.state.loading;
-        const token = this.state.token;
         return(
             <Paper>
                 <Table>
@@ -103,8 +71,8 @@ class Dashboard extends Component {
                         </TableRow>
                     </TableHead>
                 </Table>
-                <div>{ new NestedList().renderData("data", this.state.user) }</div>
-                <div><Button variant="contained" color="primary" onClick={this.createProject.bind(this)}>create Project</Button></div>
+                <div>{ new NestedList().renderData("data", this.state.user) }
+                </div>
                 {loading || this.state.userData === undefined ? (
                     <div className={'row text-center'}>
                         <span className="fa fa-spin fa-spinner fa-4x"></span>
@@ -114,6 +82,10 @@ class Dashboard extends Component {
                         { new NestedList().renderData("data", {projects: this.state.userData.projects}) }
                     </div>
                 )}
+                <div>
+                <Button variant="contained" color="primary" onClick={this.createProject.bind(this)}>create Project</Button>
+                <Button variant="contained" color="primary" onClick={this.editProfile.bind(this)}>edit Profile</Button>
+                </div>
             </Paper>
         )
     }
