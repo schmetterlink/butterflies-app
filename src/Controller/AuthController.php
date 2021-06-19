5784d591c1bc09c7dd5f82e65b2126b251e1a08d
Timor Kodal <timor@kodal.de>
2021-06-19 11:01:57 +0200

    implement form validation during user registration and return proper statuscodes: 200 if registration succeeded, 400 (bad request) in case of validation errors

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
@@ -12,6 +13,9 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Firebase\JWT\JWT;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\VarDumper\VarDumper;
 
 class AuthController extends AbstractController
 {
@@ -28,7 +32,7 @@ class AuthController extends AbstractController
     /**
      * @Route("/auth/register", name="auth_register", methods={"POST"})
      */
-    public function register(Request $request, UserPasswordEncoderInterface $encoder)
+    public function register(Request $request, UserPasswordEncoderInterface $encoder, ValidatorInterface $validator): JsonResponse
     {
         $password = $request->get('password');
         $email = $request->get('email');
@@ -40,15 +44,30 @@ class AuthController extends AbstractController
             $password = $parameters['password'] ?: '';
         }
         $user = new User();
+
         $user->setPassword($encoder->encodePassword($user, $password));
         $user->setEmail($email);
         $user->setName($name);
+
+        $errors = $validator->validate($user);
+
+        if (count($errors) > 0) {
+            //$errorsString = (string) $errors;
+            $errorList = [];
+            /** @var ConstraintViolation $error */
+            foreach ($errors as $error) {
+                $errorList[$error->getPropertyPath()][] = $error->getMessageTemplate();
+            }
+            return new JsonResponse($errorList, 400);
+        }
+
+
         $em = $this->getDoctrine()->getManager();
         $em->persist($user);
         $em->flush();
-        return $this->json([
+        return new JsonResponse([
             'user' => $user->getEmail()
-        ]);
+        ], 200);
     }
     /**
      * @Route("/auth/logout", name="auth_logout", methods={"POST", "GET"})
