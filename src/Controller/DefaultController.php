dcdfae9c04f58f2ceec121b2eb0fec1dc0187136
Timor Kodal <timor@kodal.de>
2021-06-22 11:04:58 +0200

    introduce simple form and service to handle file uploads (as suggested here: https://weenesta.com/en/blog/post/symfony-5-upload-file )

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Form\FileUploadType;
 use App\Security\JwtAuthenticator;
+use App\Service\FileUploader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,6 +13,7 @@ use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationCredentialsNotFoundException;
+use Symfony\Component\VarDumper\VarDumper;
 
 class DefaultController extends AbstractController
 {
@@ -31,6 +34,7 @@ class DefaultController extends AbstractController
         ];
         return $this->render('default/dashboard.html.twig', $data);
     }
+
     /**
      * @Route("/logout", name="logout", defaults={"reactRouting": null})
      */
@@ -40,6 +44,7 @@ class DefaultController extends AbstractController
         $session->clear();
         return $this->redirect('/login');
     }
+
     /**
      * @Route("/redirect", name="redirect", defaults={"reactRouting": null})
      */
@@ -52,109 +57,39 @@ class DefaultController extends AbstractController
                 "message" => $request->get('message')
             ]);
     }
+
+    /**
+     * @Route("/test-upload", name="app_test_upload")
+     */
+    public function testUpload(Request $request, FileUploader $file_uploader)
+    {
+        $form = $this->createForm(FileUploadType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $file = $form['upload_file']->getData();
+            if ($file) {
+                $file_name = $file_uploader->upload($file);
+                if (null !== $file_name) // for example
+                {
+                    $directory = $file_uploader->getTargetDirectory();
+                    $full_path = $directory . '/' . $file_name;
+                    echo $full_path;
+                } else {
+                    // Oups, an error occured !!!
+                }
+            }
+        }
+        return $this->render('test-upload.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
     /**
      * @Route("/{reactRouting}", name="home", defaults={"reactRouting": null})
      */
     public function index(Request $request)
     {
-        return $this->render('default/index.html.twig', ["headers"=>$request->headers] );
+        return $this->render('default/index.html.twig', ["headers" => $request->headers]);
     }
-    /**
-     * @Route("/api/users", name="users")
-     * @return \Symfony\Component\HttpFoundation\JsonResponse
-     */
-    public function getUsers()
-    {
-        $users = [
-            [
-                'id' => 1,
-                'name' => 'Timor Kodal',
-                'description' => 'Lorem Ipsum',
-                'imageURL' => 'build/images/tea-guevara.jpg'
-            ],
-            [
-                'id' => 2,
-                'name' => 'Hiu-Fai Lau',
-                'description' => '',
-                'imageURL' => '/images/hiu-fai.jpg'
-            ],
-            [
-                'id' => 3,
-                'name' => 'Johanna Reichenbach',
-                'description' => '',
-                'imageURL' => '/images/johanna.jpg'
-            ],
-            [
-                'id' => 4,
-                'name' => 'Linus Wetzel',
-                'description' => '',
-                'imageURL' => '/images/linus.jpg'
-            ],
-            [
-                'id' => 5,
-                'name' => 'Gregory Tumoscheid',
-                'description' => '',
-                'imageURL' => '/images/greg.jpg'
-            ]
-        ];
-        /** @var JsonResponse $response */
-        $response = new Response();
-
-        $response->headers->set('Content-Type', 'application/json');
-        $response->headers->set('Access-Control-Allow-Origin', '*');
-
-        $response->setContent(json_encode($users));
-
-        return $response;
-    }
-    /**
-     * @Route("/api/posts", name="posts")
-     * @return \Symfony\Component\HttpFoundation\JsonResponse
-     */
-    public function getPosts()
-    {
-        $posts = [
-              [
-                  "userId" => 1,
-                "id" => 1,
-                "title" => "wurst aut facere repellat provident occaecati excepturi optio reprehenderit",
-                "body" => "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-              ],
-              [
-                  "userId" => 1,
-                "id" => 2,
-                "title" => "qui est essen",
-                "body" => "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-              ],
-              [
-                  "userId" => 1,
-                "id" => 3,
-                "title" => "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-                "body" => "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-              ],
-              [
-                  "userId" => 1,
-                "id" => 4,
-                "title" => "eum et est occaecati",
-                "body" => "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
-              ],
-              [
-                  "userId" => 1,
-                "id" => 5,
-                "title" => "nesciunt quas odio",
-                "body" => "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
-              ]
-        ];
-        /** @var JsonResponse $response */
-        $response = new Response();
-
-        $response->headers->set('Content-Type', 'application/json');
-        $response->headers->set('Access-Control-Allow-Origin', '*');
-
-        $response->setContent(json_encode($posts));
-
-        return $response;
-    }
-
-
 }
