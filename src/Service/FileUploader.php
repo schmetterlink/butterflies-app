19dd2d800116b3d5f66afceea7f885badce067d2
Timor Kodal <timor@kodal.de>
2021-06-22 11:04:58 +0200

    introduce simple form and service to handle file uploads (as suggested here: https://weenesta.com/en/blog/post/symfony-5-upload-file )

---
+++ src/Service/FileUploader.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\DependencyInjection\Parameter;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\VarDumper\VarDumper;
+
+class FileUploader
+{
+    private $targetDirectory;
+    private $slugger;
+
+    /**
+     * FileUploader constructor.
+     * @param Parameter $targetDirectory
+     * @param SluggerInterface $slugger
+     */
+    public function __construct($targetDirectory, SluggerInterface $slugger)
+    {
+        $this->targetDirectory = $targetDirectory;
+        $this->slugger = $slugger;
+    }
+
+    public function upload(UploadedFile $file)
+    {
+        $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFilename = $this->slugger->slug($originalFilename);
+        $fileName = $safeFilename . '-' . uniqid() . '.' . $file->guessExtension();
+        try {
+            $file->move($this->getTargetDirectory(), $fileName);
+        } catch (FileException $e) {
+            // ... handle exception if something happens during file upload
+            return null; // for example
+        }
+        return $fileName;
+    }
+
+    /**
+     * @return Parameter
+     */
+    public function getTargetDirectory()
+    {
+        return $this->targetDirectory;
+    }
+}
