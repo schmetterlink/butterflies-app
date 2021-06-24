ea95011b269f20905e81e97bc491b6e4cc866c73
Timor Kodal <timor@kodal.de>
2021-06-24 16:32:35 +0200

    adjust handleChange() and resetData() - methods: input fields are stored at this.state.data

--- assets/js/components/BaseComponent.js
+++ assets/js/components/BaseComponent.js
@@ -11,13 +11,20 @@ class BaseComponent extends Component {
     }
 
     handleChange(e) {
-        let change = {}
-        change[e.target.name] = e.target.value
-        this.setState(change)
+        if (this.state.originalData[e.target.name] === undefined) {
+            this.state.originalData[e.target.name] = this.state.data[e.target.name];
+        }
+        if (event.target.files) {
+            console.debug("a file has been selected for upload to field '" + e.target.name + "':");
+            console.debug(event.target.files);
+            this.state.files[e.target.name] = event.target.files;
+        }
+        this.state.data[e.target.name] = e.target.value;
     }
 
     resetData(event) {
         console.debug("resetting data fields");
+        console.debug(this.state);
         for (let key in this.state.originalData) {
             this.state.data[key] = this.state.originalData[key];
         }
