4423b1fdbaad1c28a81fbe3ac4dabd90bb05a7d6
Timor Kodal <timor@kodal.de>
2021-06-29 16:58:26 +0200

    outsource object deserialization to "deserialize"-method and use it for both POST- and UPDATE-requests

--- src/Controller/CrudController.php
+++ src/Controller/CrudController.php
@@ -13,6 +13,9 @@ use Doctrine\ORM\Mapping\ClassMetadata;
 use Entity\Repository\CategoryRepository;
 use Exception;
 use Psr\Log\LoggerInterface;
+use ReflectionClass;
+use ReflectionException;
+use ReflectionParameter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -30,40 +33,135 @@ use TypeError;
 
 class CrudController extends AbstractController
 {
+    /** @var EntityManagerInterface $manager * */
+    private $manager;
+
+    /** @var TokenStorageInterface $tokenStorage */
+    private $tokenStorage;
+
+    /** @var LoggerInterface $logger ; */
+    private $logger;
+
+    /** @var ValidatorInterface $validator */
+    private $validator;
+
+    /** @var FileUploader $uplaoder */
+    private $uploader;
+
+
+    public function __construct(TokenStorageInterface $tokenStorage, EntityManagerInterface $manager, LoggerInterface $logger, ValidatorInterface $validator, FileUploader $uploader)
+    {
+        $this->manager = $manager;
+        $this->tokenStorage = $tokenStorage;
+        $this->logger = $logger;
+        $this->validator = $validator;
+        $this->uploader = $uploader;
+    }
+
+    /**
+     * @param $entity
+     * @param Request $request
+     * @param string $prefix
+     * @return \Doctrine\ORM\Mapping\Entity|JsonResponse
+     * @throws ReflectionException
+     */
+    public function deserialize($entity, Request $request, $prefix = 'UPDATE')
+    {
+        /** @var Entity\User $user */
+        $user = $this->tokenStorage->getToken()->getUser();
+
+        if (is_string($entity)) {
+            $entityClassName = $this->getEntityClassName($entity);
+
+            /** @var \Doctrine\ORM\Mapping\Entity $entity */
+            $entity = new $entityClassName($user);
+        }
+
+        $reflectionClass = new ReflectionClass($entity);
+        $entityName = $reflectionClass->getShortName();
+        $id = $entity->getId();
+
+        try {
+            /* parse multipart/form-data content if possible */
+            $parsed = $this->uploader->digestFormContent($request, FileUploader::WRITE_CONTENT, "{path}$entityName/$id-{fieldname}.{extension}", "{upload}$entityName/$id-{fieldname}.*");
+            $values = [];
+            $this->logger->debug("multipart/form-data successfully digested...");
+            foreach ($parsed as $parameters) {
+                $values[$parameters['name']] = $parameters['content'] ?? $parameters['path'] ?? '';
+            }
+        } catch (BadRequestException $e) {
+            $this->logger->error($e);
+            $this->logger->debug("trying to fall back to default request");
+            //return new JsonResponse(["headers"=>$request->headers->all()], 400);
+            /* fall back to default request */
+            try {
+                $values = $request->request->all() ?: $request->toArray();
+            } catch (BadRequestException $e) {
+                return new JsonResponse(["error" => $e->getMessage()], 400);
+            }
+
+        }
+
+        if (isset($values['id']) && $values['id'] != $entity->getId()) {
+            return new JsonResponse(["error" => "data manipulation error: primary keys may not be altered"], 400);
+        }
+
+        foreach ($values as $key => $value) {
+            $methodName = "set" . ucfirst($key);
+            try {
+                if (method_exists($entity, $methodName)) {
+                    $entity->$methodName($value);
+                    $this->logger->debug(
+                        "$prefix for {entity}.{$key} succeeded; value '{$value}' was set.",
+                        ["entity" => $entity::class, "key" => $key, "value" => $value]
+                    );
+                } else {
+                    $this->logger->warning(
+                        "$prefix for {entity}.{$key} failed. Field could not be set to {$value} - possibly due to a missing setter?",
+                        ["entity" => $entity::class, "key" => $key, "value" => $value]
+                    );
+                }
+            } catch (TypeError $e) {
+                /* create and assign instance from given entity id */
+                $param = new ReflectionParameter(array($entity, $methodName), 0);
+                $parameterType = $param?->getType()?->getName();
+
+                $relatedEntity = $this->manager->find($parameterType, (int)$value);
+
+                $entity->$methodName($relatedEntity);
+            } catch (Exception $e) {
+                $this->logger->error(
+                    "$prefix for {entity}.{$key} to '{value}' failed. Exception thrown: {errorMessage}",
+                    ["entity" => $entity::class, "key" => $key, "value" => $value, "errorMessage" => $e->getMessage()]
+                );
+            }
+        }
+
+        if (method_exists($entity, "setUser")) {
+            $entity->setUser($user);
+        }
+
+        return $entity;
+
+    }
+
     /**
      * @Route("/api/{entityName}", name="api_crud_add", methods={"POST"})
      */
-    public function add(string $entityName, Request $request, TokenStorageInterface $tokenStorage, EntityManagerInterface $manager):JsonResponse
+    public function add(string $entityName, Request $request): JsonResponse
     {
-        /** @var Entity\User $user */
-        $user = $tokenStorage->getToken()->getUser();
-
-        $entityClassName = $this->getEntityClassName($entityName);
-
-        /** @var \Doctrine\ORM\Mapping\Entity $entity */
-        $entity = new $entityClassName($user);
-
-        $values = $request->request->all() ?: $request->toArray();
-
-        foreach ($values as $key=>$value) {
-            $methodName = "set".ucfirst($key);
-            $entity->$methodName($value);
-        }
-
-        if (method_exists($entity, "setUserId")) {
-            $entity->setUserId($user->getId());
-        }
+        $entity = $this->deserialize($entityName, $request);
 
         /** TODO: check permissions and ownership before creating entity */
-        $manager->persist($entity);
-        $manager->flush();
+        $this->manager->persist($entity);
+        $this->manager->flush();
         return new JsonResponse($entity);
     }
 
     /**
      * @Route("/api/meta/{entityNames}/{fieldNames}", name="api_crud_get_meta", methods={"GET"}, defaults={"fieldNames"=""})
      */
-    public function getMetaData(string $entityNames, string $fieldNames, EntityManagerInterface $em, ValidatorInterface $validator): JsonResponse
+    public function getMetaData(string $entityNames, string $fieldNames): JsonResponse
     {
         $entityNames = explode(",", $entityNames);
         $fieldNames = array_filter(explode(",", $fieldNames));
@@ -74,7 +172,7 @@ class CrudController extends AbstractController
             $entityClassName = $this->getEntityClassName($entityName);
 
             /** @var ClassMetadata $meta */
-            $meta = $em->getClassMetadata($entityClassName);
+            $meta = $this->manager->getClassMetadata($entityClassName);
 
             $mappings = $meta->fieldMappings;
 
@@ -86,7 +184,7 @@ class CrudController extends AbstractController
             $metaData[$entityName] = $mappings;
 
             /** @var MetadataInterface $constraints */
-            $constraints = $validator->getMetadataFor($entityClassName);
+            $constraints = $this->validator->getMetadataFor($entityClassName);
             /**
              * @var  $field
              * @var PropertyMetadata $propertyMeta
@@ -110,72 +208,24 @@ class CrudController extends AbstractController
     /**
      * @Route("/api/{entityName}/{id}", name="api_crud_update", methods={"PUT"})
      */
-    public function update(string $entityName, string $id, Request $request, TokenStorageInterface $tokenStorage, EntityManagerInterface $manager, FileUploader $fileUploader, LoggerInterface $logger): JsonResponse
+    public function update(string $entityName, string $id, Request $request, FileUploader $fileUploader): JsonResponse
     {
         /** @var Entity\User $user */
-        $user = $tokenStorage->getToken()->getUser();
-
-        //$entity = Entity\Project::class;
-        $entityClassName = $this->getEntityClassName($entityName);
+        $user = $this->tokenStorage->getToken()->getUser();
 
         /** @var \Doctrine\ORM\Mapping\Entity $entity */
-        $entity = $this->getDoctrine()->getRepository($entityClassName)->find($id);
+        $entity = $this->getDoctrine()->getRepository($this->getEntityClassName($entityName))->find($id);
 
         if (!$entity) {
             return new JsonResponse(["error" => "resource is not available"], 404);
         }
 
-        try {
-            /* parse multipart/form-data content if possible */
-            $parsed = $fileUploader->digestFormContent($request, FileUploader::WRITE_CONTENT, "{path}$entityName/$id-{fieldname}.{extension}", "{upload}$entityName/$id-{fieldname}.*");
-            $values = [];
-            $logger->debug("multipart/form-data successfully digested...");
-            foreach ($parsed as $parameters) {
-                $values[$parameters['name']] = $parameters['content'] ?? $parameters['path'] ?? '';
-            }
-        } catch (BadRequestException $e) {
-            $logger->error($e);
-            $logger->debug("trying to fall back to default request");
-            //return new JsonResponse(["headers"=>$request->headers->all()], 400);
-            /* fall back to default request */
-            try {
-                $values = $request->request->all() ?: $request->toArray();
-            } catch (BadRequestException $e) {
-                return new JsonResponse(["error" => $e->getMessage()], 400);
-            }
-
-        }
-
-        if (isset($values['id']) && $values['id'] != $entity->getId()) {
-            return new JsonResponse(["error" => "data manipulation error: primary keys may not be altered"], 400);
-        }
-
-        foreach ($values as $key => $value) {
-            $methodName = "set" . ucfirst($key);
-            try {
-                if (method_exists($entity, $methodName)) {
-                    $entity->$methodName($value);
-                } else {
-                    $logger->warning(
-                        "UPDATE for {entity}.{$key} failed. Field could not be set to {$value} - possibly due to a missing setter?",
-                        ["entity" => $entityName, "key" => $key, "value" => $value]
-                    );
-                }
-            } catch (Exception | TypeError $e) {
-                $logger->error(
-                    "UPDATE for {entity}.{$key} to '{value}' failed. Exception thrown: {errorMessage}",
-                    ["entity" => $entityName, "key" => $key, "value" => $value, "errorMessage" => $e->getMessage()]
-                );
-            }
-        }
-        if (method_exists($entity, "setUserId")) {
-            $entity->setUserId($user->getId());
-        }
+        $entity = $this->deserialize($entity, $request, 'UPDATE');
 
         /** TODO: check permissions and ownership before updating entity */
-        $manager->persist($entity);
-        $manager->flush();
-        return new JsonResponse(["data" => $values, "headers" => $request->headers->all()], 201);
+        $this->manager->persist($entity);
+        $this->manager->flush();
+        return new JsonResponse(["data" => $entity, "headers" => $request->headers->all()], 201);
 
     }
     private function getEntityClassName($entityName):? string {
@@ -191,13 +241,13 @@ class CrudController extends AbstractController
     /**
      * @Route("/api/{entityName}/{id}", name="api_crud_delete", methods={"DELETE"})
      */
-    public function delete(string $entityName, string $id, Request $request, TokenStorageInterface $tokenStorage, EntityManagerInterface $manager):JsonResponse
+    public function delete(string $entityName, string $id, Request $request): JsonResponse
     {
         /** @var Entity\User $user */
-        $user = $tokenStorage->getToken()->getUser();
+        $user = $this->tokenStorage->getToken()->getUser();
 
-        $id = $id ?: $request->request->get('id',$request->get('id'));
-        $entityName = $entityName ?: $request->request->get('entityName',$request->get('entityName'));
+        $id = $id ?: $request->request->get('id', $request->get('id'));
+        $entityName = $entityName ?: $request->request->get('entityName', $request->get('entityName'));
 
         //$entity = Entity\Project::class;
         $entityClassName = $this->getEntityClassName($entityName);
@@ -206,8 +256,8 @@ class CrudController extends AbstractController
         $project = $this->getDoctrine()->getRepository($entityClassName)->find($id);
 
         /** TODO: check permissions and ownership before deleting entity */
-        $manager->remove($project);
-        $manager->flush();
+        $this->manager->remove($project);
+        $this->manager->flush();
         return new JsonResponse([]);
     }
 }
